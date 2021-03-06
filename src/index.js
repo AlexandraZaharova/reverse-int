10c058c59153d34873ca8157b5d0165c7351d05a
AlexandraZaharova <zakharova911@gmail.com>
2021-03-06 17:21:52 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = '' + n;
+    let newN = str.split('').reverse();
+    if (isNaN(+newN[newN.length-1])) {
+        newN.pop();
+        newN = +newN.join('');
+    } else newN = +newN.join('');
+    return newN;
 }
